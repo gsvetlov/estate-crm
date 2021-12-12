7d75fd79cb86d5ffce87611f8af07185a5b25970
Georg Svetlov <gsvetlov@gmail.com>
2021-12-12 21:19:46 +0300

    Initial commit

---
+++ src/main/java/ru/svetlov/estatecrm/EstateCrmApplication.java
@@ -0,0 +1,13 @@
+package ru.svetlov.estatecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstateCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/svetlov/estatecrm/EstateCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.svetlov.estatecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
